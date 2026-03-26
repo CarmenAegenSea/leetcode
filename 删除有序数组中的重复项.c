008428ab12ff2e6fc4e7ecc1cc8c9b4ee55c2c84
Carmen <2261689550@qq.com>
2026-03-26 18:17:19 +0800

    20260326

--- 删除有序数组中的重复项.c
+++ 删除有序数组中的重复项.c
@@ -1,8 +1,11 @@
 int removeDuplicates(int* nums, int numsSize) {
     int n = 1;
+
+    if(numsSize == 0) return 0;
+
     for(int i = 1; i < numsSize; i++){
-        if(nums[n] != nums[i]){
-            nums[n++] = nums[i++];
+        if(nums[n - 1] != nums[i]){
+            nums[n++] = nums[i];
         }
     }
 
