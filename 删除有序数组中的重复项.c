8a0eaa1ec0a59695db27cb39f06b128fa1bb655e
Carmen <2261689550@qq.com>
2026-03-27 20:21:55 +0800

    添加了注釋

--- 删除有序数组中的重复项.c
+++ 删除有序数组中的重复项.c
@@ -1,10 +1,10 @@
 int removeDuplicates(int* nums, int numsSize) {
     int n = 1;
 
-    if(numsSize == 0) return 0;
+    if(numsSize == 0) return 0; //如果數組爲空， 直接返回0
 
     for(int i = 1; i < numsSize; i++){
-        if(nums[n - 1] != nums[i]){
+        if(nums[n - 1] != nums[i]){ //如果當前元素與前一個元素不相同，則將當前元素放到新數組的末尾
             nums[n++] = nums[i];
         }
     }
--- 删除有序数组中的重复项2.c
+++ 删除有序数组中的重复项2.c
@@ -1,10 +1,10 @@
 int removeDuplicates(int* nums, int numsSize) {
     int n = 2;
 
-    if(numsSize < 3) return numsSize;
+    if(numsSize < 3) return numsSize; //如果數組長度小於3，則直接返回數組長度
 
     for(int i = 2; i < numsSize; i++){
-        if(nums[n - 2] != nums[i]){
+        if(nums[n - 2] != nums[i]){ // 如果當前元素與前兩個元素不相同，則將當前元素放到新數組的末尾
             nums[n++] = nums[i];
         }
     }
