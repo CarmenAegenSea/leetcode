ef5e87511b353ee1f883a604f04e23d9ca9b194b
Carmen <2261689550@qq.com>
2026-03-31 21:28:52 +0800

    20260331

--- leetcode/最后一个单词的长度.c
+++ leetcode/最后一个单词的长度.c
@@ -2,14 +2,9 @@ int lengthOfLastWord(char *s) {
     int length = 0;
     int i = strlen(s) - 1;
 
-    while (i >= 0 && s[i] == ' ') {
-        i--;
-    }
+    while (i >= 0 && s[i] == ' ') {i--;}
 
-    while (i >= 0 && s[i] != ' ') {
-        length++;
-        i--;
-    }
+    while (i >= 0 && s[i] != ' ') {length++;i--;}
 
     return length;
 }
