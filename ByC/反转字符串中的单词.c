633b2629ad8cfc8682d65d2328a0455c3c6e7151
CarmenAegenSea <2261689550@qq.com>
2026-05-01 00:26:27 +0800

    20260430

--- ByC/反转字符串中的单词.c
+++ ByC/反转字符串中的单词.c
@@ -1,33 +1,37 @@
-void reverse(char *s, int l, int r) {
-    while (l < r) {
-        char t = s[l];
-        s[l] = s[r];
-        s[r] = t;
-        l++, r--;
+void change(char *s, int f, int l){
+    for(; f < l; f++, l--){
+        int t = s[f]; s[f] = s[l]; s[l] = t;
     }
 }
 
 char* reverseWords(char* s) {
-    int n = strlen(s), l = 0;
-    reverse(s, 0, n - 1); // 整体翻转
-    for (int i = 0; i < n; ++i) { // 逐个单词翻转
-        while (i < n  && s[i] != ' ') i++;
-        reverse(s, l, i - 1);
-        l = i + 1;
-    }
-    
-    char *ans = malloc(sizeof(char) * (n + 1));
-    int i = 0, j = n - 1, size = 0;
-    while (i < n && s[i] == ' ') i++; // 消除首部的空格 
-    while (j >= 0 && s[j] == ' ') j--; // 消除尾部的空格
-    while (i <= j) { 
-        if (i > 0 && s[i] == ' ' && s[i - 1] == ' ') { // 单词间只留一个空格
-            i++;
-            continue;
+    int length = strlen(s);
+    change(s, 0, strlen(s) - 1);
+
+    int f = 0, l = length - 1;
+    while(s[f] == ' ') f++;
+    while(s[l] == ' ') l--;
+    int fi = f, li = l;
+
+    for(int lf = f; lf < l + 1; lf++){
+        if(s[lf] == ' ' || lf == l){
+            change(s, f, lf - (lf == l ? 0 : 1));
+            f = lf + 1;
         }
-        ans[size++] = s[i++];
     }
 
-    ans[size] = '\0';
-    return ans;
+    char *ts = malloc(length + 1);
+    int idx = 0;
+    for(int i = 0; s[i] != '\0'; i++){
+        if(s[i] != ' '){
+            ts[idx++] = s[i];
+        } else if(idx > 0 && ts[idx-1] != ' '){
+            ts[idx++] = ' ';
+        }
+    }
+    
+    if(idx > 0 && ts[idx-1] == ' ') idx--;
+    ts[idx] = '\0';
+        
+    return ts;
 }
