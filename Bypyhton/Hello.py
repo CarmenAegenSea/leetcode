0551a8b565b602f546f941c7478b8e13273b322b
CarmenAegenSea <2261689550@qq.com>
2026-06-12 12:49:21 +0800

    fead:Hello.py

--- Bypyhton/Hello.py
+++ Bypyhton/Hello.py
@@ -1,2 +1,15 @@
+import string
+import time
 
-print("Hello, World!")
+text = 'Hello World!!!'
+temp  = ' '
+for ch in text:
+    for i in string.printable:
+        if i == ch:
+            time.sleep(0.02)
+            print(temp + i)
+            temp += ch
+            break
+        else:
+            time.sleep(0.02)
+            print(temp + i)
