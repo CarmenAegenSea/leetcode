fbe336d01ec62b1d6e3cd584e95c5dacfd7aaeda
= <=>
2026-07-21 12:00:50 +0800

    fead：添加了加油站的python 题解

---
+++ Bypyhton/加油站.py
@@ -0,0 +1,24 @@
+ 
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        for i in range(len(gas)):
+            cost[i] = gas[i] - cost[i]
+        
+        begin = 0
+        while begin < len(cost):
+            if cost[begin] < 0:
+                begin += 1
+                continue
+
+            timeOil = 0
+            for j in range(begin, begin + len(cost)):
+                timeOil += cost[j % len(cost)]
+                if timeOil < 0:
+                    begin = j + 1
+                    break
+            else:
+                return begin
+
+        return -1
+    
