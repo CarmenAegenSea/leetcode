e552488dfa879f2a87fe0072c3a8ef9b2783b8d0
= <=>
2026-07-22 14:23:17 +0800

    fix:修复了'Z形变换'的c题解

--- ByC/Z型变换.c
+++ ByC/Z型变换.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 char* convert(char* s, int numRows) {
-    int length = strlen(s);
-    if (numsRows == 1 || numRows >= length) {
-        return s;
+    int len = strlen(s);
+    if (numRows < 2 || len <= numRows) {
+        char* res = (char*)malloc((len + 1) * sizeof(char));
+        strcpy(res, s);
+        return res;
     }
 
-    char* returnS = (char*)calloc(length, ' ');
-    int T = 2 * (numRows - 1);
-    for (int i = 0; i < numRows; i++) {`
-        for (int j = 0; j < length; j++) {
-            
-        }
+    char** rows = (char**)malloc(numRows * sizeof(char*));
+    int* rowLen = (int*)calloc(numRows, sizeof(int));
+    for (int i = 0; i < numRows; i++) {
+        rows[i] = (char*)malloc((len + 1) * sizeof(char));
+        rows[i][0] = '\0';
     }
+
+    int row = 0;
+    int dir = 1;
+    for (int i = 0; i < len; i++) {
+        rows[row][rowLen[row]++] = s[i];
+        rows[row][rowLen[row]] = '\0';
+        if (row == 0) {
+            dir = 1;
+        } else if (row == numRows - 1) {
+            dir = -1;
+        }
+        row += dir;
+    }
+
+    char* result = (char*)malloc((len + 1) * sizeof(char));
+    result[0] = '\0';
+    for (int i = 0; i < numRows; i++) {
+        strcat(result, rows[i]);
+        free(rows[i]);
+    }
+    free(rows);
+    free(rowLen);
+    return result;
 }
